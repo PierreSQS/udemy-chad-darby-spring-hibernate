fc022f919a7b1ed33a7e534361502978e1222758
PierreSQS <nomo_messina@web.de>
2022-02-11 20:36:00 +0100

    chap[401-406]: added a custom login form and corrected the SecurityConfig file

--- section47_55-security/spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/SecurityConfig.java
+++ section47_55-security/spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.luv2code.springsecurity.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -20,11 +21,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .roles("EMPLOYEE"))
 //                .and()
                 .withUser(users.username("Mary")
-                        .password("{noop}Mary")
+                        .password("Mary")
                         .roles("MANAGER"))
 //                .and()
                 .withUser(users.username("Susan")
-                        .password("{noop}Susan")
+                        .password("Susan")
                         .roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/showLoginPage")
+                    .loginProcessingUrl("/authenticateUser")
+                .permitAll();
     }
 }
