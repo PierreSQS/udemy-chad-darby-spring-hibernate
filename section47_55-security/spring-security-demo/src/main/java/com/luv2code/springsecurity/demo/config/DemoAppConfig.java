4bba1a1122570d858e25f326b9f55261048a050c
PierreSQS <nomo_messina@web.de>
2022-02-18 23:16:25 +0100

    chap442-Security JDBC: final code for JDBC-Authentication

--- section47_55-security/spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ section47_55-security/spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -1,18 +1,32 @@
 package com.luv2code.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Objects;
+
+@Slf4j
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = {"com.luv2code.springsecurity.demo"})
+@PropertySource("classpath:mysql.properties")
 public class DemoAppConfig {
-    // define a bean for ViewResolver
 
+    @Autowired
+    Environment env;
+
+    // define a bean for ViewResolver
     @Bean
     public ViewResolver viewResolver() {
 
@@ -21,5 +35,39 @@ public class DemoAppConfig {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
 
+    }
+
+    // Define a bean for our Security DataSource
+    @Bean
+    public DataSource securityDataSource() {
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set jdbc driver class
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            throw new RuntimeException(exc);
+        }
+
+        String jdbcUrlProp = env.getProperty("jdbc.url");
+        String userProp = env.getProperty("jdbc.user");
+
+        // log the connection
+        log.info("jdbc.url={}",jdbcUrlProp);
+        log.info("jdbc.user={}",userProp);
+
+        // set the database connection
+        securityDataSource.setJdbcUrl(jdbcUrlProp);
+        securityDataSource.setUser(userProp);
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection props
+        securityDataSource.setInitialPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.initialPoolSize"))));
+        securityDataSource.setMinPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.minPoolSize"))));
+        securityDataSource.setMaxPoolSize(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.maxPoolSize"))));
+        securityDataSource.setMaxIdleTime(Integer.parseInt(Objects.requireNonNull(env.getProperty("connection.pool.maxIdleTime"))));
+
+        return securityDataSource;
     }
 }
--- section47_55-security/spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/SecurityConfig.java
+++ section47_55-security/spring-security-demo/src/main/java/com/luv2code/springsecurity/demo/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.luv2code.springsecurity.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,21 +16,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String MANAGER = "MANAGER";
     private static final String ADMIN = "ADMIN";
 
+    @Autowired
+    private DataSource securityDatasource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // 5.1.0.Release implementation!!! Actually deprecated!!!!
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-                .withUser(users.username("John")
-                        .password("John")
-                        .roles(EMPLOYEE))
-                .withUser(users.username("Mary")
-                        .password("Mary")
-                        .roles( MANAGER, EMPLOYEE))
-                .withUser(users.username("Susan")
-                        .password("Susan")
-                        .roles( ADMIN, EMPLOYEE));
+        auth.jdbcAuthentication().dataSource(securityDatasource);
     }
 
     @Override
